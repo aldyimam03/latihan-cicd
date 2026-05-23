d1e7d423397daa1078e5c326f195090e8fc0996e
Aldy Imam Wijaya <aldyimam03@gmail.com>
2026-05-23 10:45:29 +0700

    add test configuration to Vite setup

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/latihan-cicd/'
-})
+  base: '/latihan-cicd/',
+  test: {
+    include: ['src/**/*.test.js'],
+    exclude: ['backend/**', 'node_modules/**', 'dist/**'],
+  },
+});
